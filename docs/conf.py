5a04fb90a529d6342fe85245bccb5b849387d9b9
Utkarsh Bhatt <utkarsh.bhatt@canonical.com>
2025-12-09 14:11:27 +0530

    docs: Add Read the Docs infrastructure
    - Add .readthedocs.yaml for RTD build configuration
    - Add Sphinx conf.py with Canonical theme and Chopsticks settings
    - Add requirements.txt with canonical-sphinx and extensions
    - Add Makefile from canonical-sphinx-docs-starter-pack
    - Add .gitignore to exclude build artifacts
    - Add .custom_wordlist.txt for project-specific terms
    
    Uses Python 3.12 and builds with Sphinx in dirhtml format.

---
+++ docs/conf.py
@@ -0,0 +1,115 @@
+import datetime
+import os
+
+# Chopsticks Documentation Configuration
+
+#######################
+# Project information #
+#######################
+
+project = "Chopsticks"
+author = "Canonical Ltd."
+copyright = "%s Apache-2.0, %s" % (datetime.date.today().year, author)
+
+html_title = project + " documentation"
+
+# Documentation website URL
+ogp_site_url = "https://chopsticks.readthedocs.io/"
+ogp_site_name = project
+ogp_image = "https://assets.ubuntu.com/v1/cc828679-docs_illustration.svg"
+
+# HTML context for templates
+html_context = {
+    "product_page": "github.com/canonical/chopsticks",
+    "discourse": "https://discourse.ubuntu.com",
+    "github_url": "https://github.com/canonical/chopsticks",
+    'repo_default_branch': 'main',
+    "repo_folder": "/docs/",
+    "display_contributors": True,
+    'github_issues': 'enabled',
+}
+
+#######################
+# Sitemap configuration
+#######################
+
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "/")
+
+if 'READTHEDOCS_VERSION' in os.environ:
+    version = os.environ["READTHEDOCS_VERSION"]
+    sitemap_url_scheme = '{version}{link}'
+else:
+    sitemap_url_scheme = 'MANUAL/{link}'
+
+sitemap_show_lastmod = True
+sitemap_excludes = [
+    '404/',
+    'genindex/',
+    'search/',
+]
+
+#############
+# Redirects #
+#############
+
+redirects = {}
+
+###########################
+# Link checker exceptions #
+###########################
+
+linkcheck_ignore = [
+    "http://127.0.0.1:8000",
+    "http://10.240.47.47:80",  # Local test endpoints
+]
+
+linkcheck_anchors_ignore_for_url = [r"https://github\.com/.*"]
+linkcheck_retries = 3
+
+########################
+# Configuration extras #
+########################
+
+extensions = [
+    "canonical_sphinx",
+    "notfound.extension",
+    "sphinx_design",
+    "sphinx_reredirects",
+    "sphinx_tabs.tabs",
+    "sphinxcontrib.jquery",
+    "sphinxext.opengraph",
+    "sphinx_config_options",
+    "sphinx_contributor_listing",
+    "sphinx_filtered_toctree",
+    "sphinx_related_links",
+    "sphinx_roles",
+    "sphinx_terminal",
+    "sphinx_ubuntu_images",
+    "sphinx_youtube_links",
+    "sphinxcontrib.cairosvgconverter",
+    "sphinx_last_updated_by_git",
+    "sphinx.ext.intersphinx",
+    "sphinx_sitemap",
+]
+
+exclude_patterns = [
+    "doc-cheat-sheet*",
+]
+
+rst_epilog = """
+.. include:: /reuse/links.txt
+"""
+
+rst_prolog = """
+.. role:: center
+   :class: align-center
+"""
+
+# Workaround for https://github.com/canonical/canonical-sphinx/issues/34
+if "discourse_prefix" not in html_context and "discourse" in html_context:
+    html_context["discourse_prefix"] = html_context["discourse"] + "/t/"
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    'locust': ('https://docs.locust.io/en/stable/', None),
+}
